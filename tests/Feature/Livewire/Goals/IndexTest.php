beabfec6f6270af06617a8ba9d24473d3e741b6e
Jean-Marc Strauven <jms@grazulex.be>
2025-04-30 13:02:30 +0200

    fixe test

--- tests/Feature/Livewire/Goals/IndexTest.php
+++ tests/Feature/Livewire/Goals/IndexTest.php
@@ -1,9 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Livewire\Goals\Index;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
     Livewire::test(Index::class)
         ->assertStatus(200);
 });
