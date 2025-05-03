5f7b28a0ddfcb6f19643cc40108b6d8fa026f9d2
Jean-Marc Strauven <jms@grazulex.be>
2025-05-03 18:32:12 +0200

    add new tests

--- tests/Feature/Livewire/Goals/IndexTest.php
+++ tests/Feature/Livewire/Goals/IndexTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Livewire\Goals\Index;
+use App\Models\Goal;
 use App\Models\User;
 use Livewire\Livewire;
 
@@ -12,4 +13,69 @@ it('renders successfully', function () {
 
     Livewire::test(Index::class)
         ->assertStatus(200);
+});
+
+test('show goals', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $goal = Goal::factory()->create(['user_id' => $user->id]);
+
+    Livewire::test(Index::class)
+        ->assertSee($goal->name)
+        ->assertSee($goal->description);
+});
+
+test('can paginate above 10 goals', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Goal::factory()->count(15)->create(['user_id' => $user->id]);
+
+    Livewire::test(Index::class)
+        ->assertSee('Showing 1 to 10 of 15 results');
+});
+
+test('can go to second page if pagination is available', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Goal::factory()->count(15)->create(['user_id' => $user->id]);
+
+    Livewire::test(Index::class)
+        ->call('gotoPage', 2)
+        ->assertSee('Showing 11 to 15 of 15 results');
+});
+
+test('can delete goal', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $goal = Goal::factory()->create(['user_id' => $user->id]);
+
+    Livewire::test(Index::class)
+        ->call('delete', $goal->id);
+
+    expect(Goal::find($goal->id))->toBeNull();
+});
+
+test('can dispatch edit event', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $goal = Goal::factory()->create(['user_id' => $user->id]);
+
+    Livewire::test(Index::class)
+        ->call('edit', $goal->id)
+        ->assertDispatched('editGoal', $goal->id);
+});
+
+test('can relead goals', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Livewire::test(Index::class)
+        ->call('reloadGoals')
+        ->assertStatus(200);
+
 });
--- tests/Feature/Livewire/Topics/CreateTest.php
+++ tests/Feature/Livewire/Topics/CreateTest.php
@@ -2,10 +2,41 @@
 
 declare(strict_types=1);
 
+use App\Enums\UnitEnum;
 use App\Livewire\Topics\Create;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(Create::class)
         ->assertStatus(200);
+});
+
+test('can create topic', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Livewire::test(Create::class)
+        ->set('name', 'Test Topic')
+        ->set('description', 'This is a test topic.')
+        ->set('unit', UnitEnum::calories->value)
+        ->call('submit');
+
+    $this->assertDatabaseHas('topics', [
+        'name' => 'Test Topic',
+        'description' => 'This is a test topic.',
+        'unit' => UnitEnum::calories->value,
+    ]);
+});
+
+test('need to fill all fields', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Livewire::test(Create::class)
+        ->set('name', '')
+        ->set('description', '')
+        ->set('unit', '')
+        ->call('submit')
+        ->assertHasErrors(['name', 'description', 'unit']);
 });
