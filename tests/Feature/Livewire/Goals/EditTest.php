e21f338f885c2307e85110ff34bb2d50198e7479
Jean-Marc Strauven <jms@grazulex.be>
2025-05-03 18:57:42 +0200

    add tests

--- tests/Feature/Livewire/Goals/EditTest.php
+++ tests/Feature/Livewire/Goals/EditTest.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Enums\GoalTypeEnum;
 use App\Livewire\Goals\Edit;
+use App\Models\Topic;
 use App\Models\User;
 use Livewire\Livewire;
 
@@ -12,4 +14,72 @@ it('renders successfully', function () {
 
     Livewire::test(Edit::class)
         ->assertStatus(200);
+});
+
+test('can see data from goal', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+    $topic = Topic::factory()->create();
+    $goal = $topic->goals()->create([
+        'name' => 'Test Goal',
+        'topic_id' => $topic->id,
+        'user_id' => $user->id,
+        'type' => GoalTypeEnum::decrease,
+        'target' => 100,
+        'started_at' => now(),
+        'ended_at' => now()->addDays(30),
+    ]);
+
+    Livewire::test(Edit::class)
+        ->call('edit', $goal->id)
+        ->assertSet('name', 'Test Goal')
+        ->assertSet('topic_id', $topic->id)
+        ->assertSet('type', GoalTypeEnum::decrease->value)
+        ->assertSet('target', 100);
+
+    expect($goal->name)->toBe('Test Goal');
+    expect($goal->type)->toBe(GoalTypeEnum::decrease);
+    expect($goal->target)->toBe(100);
+});
+
+test('can update attribute', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+    $topic = Topic::factory()->create();
+    $goal = $topic->goals()->create([
+        'name' => 'Test Goal',
+        'topic_id' => $topic->id,
+        'user_id' => $user->id,
+        'type' => GoalTypeEnum::decrease,
+        'target' => 100,
+        'started_at' => now(),
+        'ended_at' => now()->addDays(30),
+    ]);
+
+    $attributes = [
+        'name' => 'Updated Goal',
+        'topic_id' => $topic->id,
+        'type' => GoalTypeEnum::decrease,
+        'target' => 200,
+        'started_at' => now(),
+        'ended_at' => now()->addDays(30),
+    ];
+
+    Livewire::test(Edit::class)
+        ->call('edit', $goal->id)
+        ->set('name', $attributes['name'])
+        ->set('topic_id', $attributes['topic_id'])
+        ->set('type', $attributes['type'])
+        ->set('target', $attributes['target'])
+        ->set('started_at', $attributes['started_at'])
+        ->set('ended_at', $attributes['ended_at'])
+        ->call('submit');
+
+    $goal->refresh();
+
+    expect($goal->name)->toBe($attributes['name']);
+    expect($goal->topic_id)->toBe($attributes['topic_id']);
+    expect($goal->type)->toBe($attributes['type']);
+    expect($goal->target)->toBe($attributes['target']);
+
 });
