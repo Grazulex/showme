cab4050d0025824aabcf2af90e595dfae443d690
Jean-Marc Strauven <jms@grazulex.be>
2025-05-06 05:33:45 +0200

    change size badge avg

--- resources/views/livewire/topics/avg-values.blade.php
+++ resources/views/livewire/topics/avg-values.blade.php
@@ -2,11 +2,11 @@
     @if ($label)
         <div>
             @if ($slope > 0.05)
-                <flux:badge color="green" size="xs">{{$label}}%</flux:badge>
+                <flux:badge color="green" size="sm">{{$label}}%</flux:badge>
             @elseif ($slope < -0.05)
-                <flux:badge color="red" size="xs">{{$label}}%</flux:badge>
+                <flux:badge color="red" size="sm">{{$label}}%</flux:badge>
             @else
-                <flux:badge color="blue" size="xs">{{$label}}%</flux:badge>
+                <flux:badge color="blue" size="sm">{{$label}}%</flux:badge>
             @endif
         </div>
     @endif
