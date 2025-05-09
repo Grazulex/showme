d377acd36b7d033d57dc7d20d171607640d7e292
Jean-Marc Strauven <jms@grazulex.be>
2025-05-09 06:43:53 +0200

    add user authentication to Resume component test

--- tests/Feature/Livewire/Dashboard/ResumeTest.php
+++ tests/Feature/Livewire/Dashboard/ResumeTest.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 use App\Livewire\Dashboard\Resume;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
     Livewire::test(Resume::class)
         ->assertStatus(200);
 });
