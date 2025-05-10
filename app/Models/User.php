f76dc37ed1ca920a34e1e1858aadf99819d30bcb
Jean-Marc Strauven <jms@grazulex.be>
2025-05-11 00:45:54 +0200

    refactor: remove unused BelongsToMany import from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\DatabaseNotification;
